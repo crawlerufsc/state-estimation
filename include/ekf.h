aac10485c996fc4dbc9df82adbd13c16dd71cd0a
bruno-szdl <bruno.szdl@gmail.com>
2021-06-28 21:11:58 -0300

    ekf.h: Modified ekf.h

--- include/ekf.h
+++ include/ekf.h
@@ -4,7 +4,8 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string>
-#include <vector>
+#include <array>
+#include <Eigen/dense>
 
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
@@ -17,30 +18,42 @@ public:
     
 private:
     ros::NodeHandle nh_;
-    
+
+    nav_msgs::Odometry ekf_state_msg_;
+
+    Eigen::Matrix<float, 5, 1> current_state_;
+      
+    //imu variables
+    std::array<float, 4> imu_orientation_;
+    std::array<float, 9> imu_orientation_covariance_;
+    std::array<float, 3> imu_angular_velocity_;
+    std::array<float, 9> imu_angular_velocity_covariance_;
+    std::array<float, 3> imu_linear_acceleration_;
+    std::array<float, 9> imu_linear_acceleration_covariance_;
+
+    //gps variables
+    std::array<float, 3> gps_position_;
+    std::array<float, 3> gps_position_covariance_;
+    std::array<float, 3> gps_velocity_;
+    std::array<float, 3> gps_velocity_covariance_;
+
+    //wheel encoder variables
+    std::array<float, 3> wheel_encoder_position_;
+    std::array<float, 3> wheel_encoder_position_covariance_;
+    std::array<float, 3> wheel_encoder_velocity_;
+    std::array<float, 3> wheel_encoder_velocity_covariance_;
+
+    //ekf state variables
+    std::array<float, 3> ekf_position_;
+    std::array<float, 3> ekf_position_covariance_;
+    std::array<float, 3> ekf_velocity_;
+    std::array<float, 3> ekf_velocity_covariance_;
+
     //subscribers and publishers
     ros::Subscriber imu_sub;
     ros::Subscriber gps_sub;
     ros::Subscriber wheel_encoder_sub;
     ros::Publisher  ekf_state_pub_;
-    
-    //imu variables
-    std::vector<float> quaternion_orientation_;
-    std::vector<float> quaternion_orientation_covariance_;
-    std::vector<float> angular_velocity_;
-    std::vector<float> angular_velocity_covariance_;
-    std::vector<float> linear_velocity_;
-    std::vector<float> linear_velocity_covariance_;
-
-    //gps variables
-    std::vector<float> gps_position_;
-    std::vector<float> gps_position_covariance_;
-    std::vector<float> gps_velocity_;
-    std::vector<float> gps_velocity_covariance_;
-
-    //wheel encoder variable
-    std::vector<float> wheel_velocity_;
-    std::vector<float> wheel_velocity_covariance_;
 
     //callbacks
     void imuCallback(const std_msgs::Float32& message_holder);
@@ -48,10 +61,10 @@ private:
     void wheelEncoderCallback(const std_msgs::Float32& message_holder);
 
     //methods
-    void getImuData()
-    void getGPSData()
-    void getWheelEnconderData()
-    void filterData()
-    void publishData()
+    void getImuData();
+    void getGPSData();
+    void getWheelencoderData();
+    void filterData();
+    void publishData();
 
 #endif
